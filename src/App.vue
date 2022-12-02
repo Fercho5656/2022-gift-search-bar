fa0135bb3998d559da8e7455b6aabfb74b1c5eb1
Fercho5656 <morenomoranfernando@gmail.com>
2022-12-01 23:25:22 -0600

    Implement debounce

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,36 @@
 <template>
   <div class="w-full h-full flex flex-col gap-5 justify-center items-center">
     <h1 class="text-4xl font-bold">Gift Search Bar</h1>
-    <input type="text" class="p-2 border-2 border-gray-dark" v-model="searchTerm" placeholder="Start typing..." />
+    <input :disabled="isSearching" type="text" class="p-2 rounded-md border-2 border-gray-dark disabled:opacity-50"
+      v-model="searchTerm" placeholder="Start typing..." />
+    <spinner v-show="isSearching" />
     <ul class="list-disc">
-      <li>Display suggestions here</li>
+      <li v-for="product in searchResults?.products" :key="product.id">
+        {{ product.title }}
+      </li>
     </ul>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, watch } from 'vue'
+import spinner from './components/spinner.vue'
+import { debounce } from 'debounce'
+import findProducts from './services/findProducts';
+import IProducts from './interfaces/IProducts';
 
 const searchTerm = ref('')
+const searchResults = ref<IProducts>()
+const isSearching = ref(false)
 
-const findProducts = async (term: string) => { }
+const onSearchTermChange = debounce(async (term: string) => {
+  isSearching.value = true
+  searchResults.value = await findProducts(term)
+  isSearching.value = false
+}, 300)
 
-watch(searchTerm, newTerm => findProducts(newTerm))
+watch(searchTerm, (newTerm: string) => {
+  if (newTerm === '') return searchResults.value = undefined
+  onSearchTermChange(newTerm)
+})
 </script>
